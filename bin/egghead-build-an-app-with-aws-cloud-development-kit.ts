358b7ca5b20a599f0568c411515b21dcf677b4f6
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-11 16:43:35 +0200

    Revert changes

--- bin/egghead-build-an-app-with-aws-cloud-development-kit.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { EggheadBuildAnAppWithAwsCloudDevelopmentKitStack } from '../lib/egghead-build-an-app-with-aws-cloud-development-kit-stack';
-
-const app = new cdk.App();
-new EggheadBuildAnAppWithAwsCloudDevelopmentKitStack(app, 'EggheadBuildAnAppWithAwsCloudDevelopmentKitStack');
