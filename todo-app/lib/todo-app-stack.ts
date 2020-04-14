9f525345a7f7cc8e01db68bdcdf877cac0ed1ffc
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-14 18:28:25 +0200

    Added the code for 'Create and deploy a lambda function with AWS CDK' lesson

--- todo-app/lib/todo-app-stack.ts
+++ todo-app/lib/todo-app-stack.ts
@@ -1,7 +1,14 @@
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class TodoAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const helloLambda = new lambda.Function(this, "HelloLambda", {
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "hello.handler",
+            runtime: lambda.Runtime.NODEJS_12_X
+        });
+    }
 }
