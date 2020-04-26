2588b8869a563cc6b32a3c495c4e7ad5381bc7ec
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-26 14:54:16 +0200

    Added code for 'Get all items from a DynamoDB table with DocumentClient' lesson

--- todo-app/lib/todo-app-stack.ts
+++ todo-app/lib/todo-app-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apiGateway from "@aws-cdk/aws-apigateway";
 import * as s3 from "@aws-cdk/aws-s3";
-import * as s3Notifications from "@aws-cdk/aws-s3-notifications";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 import { TodoBackend } from "./todo-backend";
 
@@ -12,27 +11,13 @@ export class TodoAppStack extends cdk.Stack {
 
         const todoBackend = new TodoBackend(this, "TodoBackend");
 
-        const helloLambda = new lambda.Function(this, "HelloLambda", {
-            code: lambda.Code.fromAsset("lambda"),
-            handler: "hello.handler",
-            runtime: lambda.Runtime.NODEJS_12_X,
-            memorySize: 256,
-            timeout: cdk.Duration.seconds(10),
-            environment: { isProduction: "absolutely not" }
-        });
-
         new apiGateway.LambdaRestApi(this, "Endpoint", {
-            handler: helloLambda
+            handler: todoBackend.handler
         });
 
         const logoBucket = new s3.Bucket(this, "LogoBucket", {
             publicReadAccess: true
         });
-
-        logoBucket.addEventNotification(
-            s3.EventType.OBJECT_CREATED,
-            new s3Notifications.LambdaDestination(helloLambda)
-        );
 
         new s3Deployment.BucketDeployment(this, "DeployLogo", {
             destinationBucket: logoBucket,
--- todo-app/lib/todo-backend.ts
+++ todo-app/lib/todo-backend.ts
@@ -1,12 +1,24 @@
 import * as cdk from "@aws-cdk/core";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class TodoBackend extends cdk.Construct {
+    public readonly handler: lambda.Function;
+
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id);
 
-        new dynamodb.Table(this, "TodoDatabase", {
+        const todosTable = new dynamodb.Table(this, "TodoDatabase", {
             partitionKey: { name: "id", type: dynamodb.AttributeType.STRING }
+        });
+
+        this.handler = new lambda.Function(this, "TodoHandler", {
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "todoHandler.handler",
+            runtime: lambda.Runtime.NODEJS_12_X,
+            environment: {
+                TABLE_NAME: todosTable.tableName
+            }
         });
     }
 }
