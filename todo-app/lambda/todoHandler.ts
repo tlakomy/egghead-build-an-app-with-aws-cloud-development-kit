fe0d3daea347b6278ebd816d9f3edb3ce73f4569
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-29 16:31:04 +0200

    Added code for 'Adding data to a DynamoDB table with put' lesson

--- todo-app/lambda/todoHandler.ts
+++ todo-app/lambda/todoHandler.ts
@@ -24,6 +24,23 @@ const getAllTodos = async () => {
     return scanResult;
 };
 
+const addTodoItem = async (data: { todo: string; id: string }) => {
+    const { id, todo } = data;
+    if (todo && todo !== "") {
+        await dynamo
+            .put({
+                TableName: tableName,
+                Item: {
+                    id: "totally_random_id",
+                    todo
+                }
+            })
+            .promise();
+    }
+
+    return todo;
+};
+
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
     try {
         const { httpMethod, body: requestBody } = event;
@@ -33,8 +50,22 @@ exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
 
             return createResponse(response.Items || []);
         }
+
+        if (!requestBody) {
+            return createResponse("Missing request body", 500);
+        }
+
+        const data = JSON.parse(requestBody);
+
+        if (httpMethod === "POST") {
+            const todo = await addTodoItem(data);
+            return todo
+                ? createResponse(`${todo} added to the database`)
+                : createResponse("Todo is missing", 500);
+        }
+
         return createResponse(
-            `We only accept GET requests for now, not ${httpMethod}`,
+            `We only accept GET, POST, OPTIONS and DELETE, not ${httpMethod}`,
             500
         );
     } catch (error) {
